2f2116eae3e6a261e19a8d98341335e0c4bcdf48
shivapbhusal <civabhusal@gmail.com>
2018-04-15 01:22:48 -0400

    add k nearest neighbour

--- src/kNN.py
+++ src/kNN.py
@@ -3,6 +3,7 @@ Implementation of K-Nearest Neighbour classifier
 '''
 import ast
 import math
+import profiler as profiler
 
 class KNN:
 	def __init__(self, kValue):
@@ -18,10 +19,11 @@ class KNN:
 			allDistances[distance]=existingProfile['author']
 		keys=list(allDistances.keys())
 		keys.sort()
-		neighbourClass=[]
+		kNeighbours=[]
 		for i in range(self.K):
-			neighbourClass.append(allDistances[keys[i]])
-		print(neighbourClass)
+			kNeighbours.append(allDistances[keys[i]])
+		print(kNeighbours)
+		print(self.getClass(kNeighbours))
 			
 	def getEuclidianDistance(self,newProfile, existingProfile):
 		avgLengthDiff=pow((newProfile['avgLength']-existingProfile['avgLength']),2)
@@ -29,8 +31,28 @@ class KNN:
 		distance=math.sqrt(avgLengthDiff+varianceDiff)
 		return distance
 
+	def getClass(self, kNeighbours):
+		authorDict=dict()
+		for author in kNeighbours:
+			if author not in authorDict:
+				authorDict[author]=1
+			else:
+				authorDict[author]+=1
+		print(authorDict)
+		if authorDict['Joyce']==5:
+			return 'Joyce'
+		elif authorDict['Dikens']==5:
+			return 'Dikens'
+		else:
+			if authorDict['Joyce']>authorDict['Dikens']:
+				return 'Joyce'
+			else:
+				return 'Dikens'
+
 knn=KNN(5)
-newProfile=dict()
-newProfile['avgLength']=29.096153846153847
-newProfile['variance']=7.133421400264201
-print(knn.classify(newProfile))
+p=profiler.Profiler()
+directory='testData/Joyce/test4.txt'
+newProfile=ast.literal_eval(str(p.getProfile(directory,'Joyce')))
+knn.classify(newProfile)
+
+#print(knn.classify(newProfile))
